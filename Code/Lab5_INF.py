26772006ffd0e74ddc6a1acaa057933323f3e83c
ᴍʏsᴛɪᴄs <112765359+mysticslippers@users.noreply.github.com>
2022-12-14 23:49:30 +0300

    Update Lab5_INF.py

--- Code/Lab5_INF.py
+++ Code/Lab5_INF.py
@@ -1,9 +1,18 @@
-def AdditionalTask15():
-    import pandas as pd
-    import matplotlib.pyplot as plt
+"""
+Лабораторная работа №5
+Михайлов Дмитрий Андреевич
+Группа P3118
+"""
 
-    data = pd.read_csv("AdditionalTask13.csv", delimiter=";", encoding='utf-8')
+
+def AdditionalTask15():
+    import matplotlib.pyplot as plt
+    import pandas as pd
+
+    data = pd.read_csv("AdditionalTask13.csv", sep=";")
     data.boxplot()
+
+    plt.legend(['Открытие', 'Максимум', 'Минимум', 'Закрытие'])
     plt.xticks(rotation=90)
     plt.show()
 
